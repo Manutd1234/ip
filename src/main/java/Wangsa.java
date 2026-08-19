96de8e73e383b8a5e538d536bbcc37545ccc5b90
Manutd1234 <e1525902@u.nus.edu>
2026-08-19 22:27:58 +0800

    Rename chatbot from Duke to Wangsa

---
+++ src/main/java/Wangsa.java
@@ -0,0 +1,9 @@
+/**
+ * Entry point for the Wangsa chatbot.
+ */
+public class Wangsa {
+    public static void main(String[] args) {
+        String banner = "Wangsa";
+        System.out.println(banner);
+    }
+}
