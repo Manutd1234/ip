a54b86073f8fbf145e743fe40fb99e6c7c91ce72
Manutd1234 <e1525902@u.nus.edu>
2026-08-19 22:35:13 +0800

    Add command echo and bye exit

--- src/main/java/Wangsa.java
+++ src/main/java/Wangsa.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * Entry point for the Wangsa chatbot.
  */
@@ -11,7 +13,21 @@ public class Wangsa {
         System.out.println("Hello! I'm Wangsa.");
         System.out.println("What can I do for you?");
         System.out.println(SEPARATOR);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(SEPARATOR);
+
+        try (Scanner scanner = new Scanner(System.in)) {
+            while (scanner.hasNextLine()) {
+                String command = scanner.nextLine();
+                System.out.println(SEPARATOR);
+
+                if (command.equals("bye")) {
+                    System.out.println("Bye. Hope to see you again soon!");
+                    System.out.println(SEPARATOR);
+                    break;
+                }
+
+                System.out.println(command);
+                System.out.println(SEPARATOR);
+            }
+        }
     }
 }
