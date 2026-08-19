86fb6a445af803f0547272fa7d784c20de1ddd49
Manutd1234 <e1525902@u.nus.edu>
2026-08-19 22:58:01 +0800

    Add task storage and list command

--- src/main/java/Wangsa.java
+++ src/main/java/Wangsa.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
  * Entry point for the Wangsa chatbot.
  */
 public class Wangsa {
+    private static final int MAX_TASKS = 100;
     private static final String SEPARATOR = "____________________________________________________________";
     private static final String BANNER = "Wangsa";
 
@@ -13,6 +14,9 @@ public class Wangsa {
         System.out.println("Hello! I'm Wangsa.");
         System.out.println("What can I do for you?");
         System.out.println(SEPARATOR);
+
+        String[] tasks = new String[MAX_TASKS];
+        int taskCount = 0;
 
         try (Scanner scanner = new Scanner(System.in)) {
             while (scanner.hasNextLine()) {
@@ -25,7 +29,19 @@ public class Wangsa {
                     break;
                 }
 
-                System.out.println(command);
+                if (command.equals("list")) {
+                    System.out.println("Here are the tasks in your list:");
+                    for (int i = 0; i < taskCount; i++) {
+                        System.out.println((i + 1) + ". " + tasks[i]);
+                    }
+                } else if (taskCount < MAX_TASKS) {
+                    tasks[taskCount] = command;
+                    taskCount++;
+                    System.out.println("added: " + command);
+                } else {
+                    System.out.println("Sorry, your task list is full.");
+                }
+
                 System.out.println(SEPARATOR);
             }
         }
