d5e8655e5e0703c20778a5c5d5187e5588708085
Manutd1234 <e1525902@u.nus.edu>
2026-08-20 20:38:46 +0800

    Remove pre-package source paths
    Complete the A-Packages move by deleting the obsolete default-package copies, leaving src/main/java/duke as the single source location.

--- src/main/java/Todo.java
+++
@@ -1,9 +0,0 @@
-/**
- * A task without an associated date or time.
- */
-public class Todo extends Task {
-    /** Creates a todo task with the given description. */
-    public Todo(String description) {
-        super(description, TaskType.TODO);
-    }
-}
